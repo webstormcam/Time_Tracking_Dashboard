a3073f3abb1c638ba021cac1ddc6c6a87c1bdb78
Cameron Chandler <webstormcam@gmail.com>
2022-05-04 21:57:47 -0400

    Building these weird squares lol

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Replace ./data.json with your JSON feed
-fetch('localhost:3000/data.json').then(response => {
-    return response.json();
-  }).then(data => {
-    // Work with JSON data here
-    console.log(data);
-  }).catch(err => {
-    // Do something for an error here
-  });
+// ES6/ES2015
+// app.js
+import * as data from 'data.json';
+const {name} = data;
+console.log(name); // output 'testing'
